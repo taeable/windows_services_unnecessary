a77cb11b42f27d39713108214bf2d59300858806
taeable <40120716+taeable@users.noreply.github.com>
2024-12-13 00:51:04 -0500

    Created script to automate the services
    Created a script to auto turn off the services listed. This script is tailored towards my ROG Ally.

---
+++ Turn_off_services.bat
@@ -0,0 +1,202 @@
+:: disables Alljoyn Router Service
+sc config "AJRouter" start-disabled
+
+:: disables Application Layer Gateway Service
+sc config "ALG" start-disabled
+
+:: disables ActiveX Installer
+sc config "AppXSvc" start-disabled
+
+:: disables bitlocker Drive Encryption Service
+sc config "BDESVC" start-disabled
+
+:: disables Certificate Propagation Service
+sc config "CertPropSvc" start-disabled
+
+:: disables Microsoft Office Click-to-Run Service
+sc config "ClickToRunSvc" start-disabled
+
+:: disables Microsoft (R) Diagnostics Hub Standard Collector Service
+sc config "diagnosticshub.standardcollector.service" start-disabled
+
+:: disables Diagnostic Execution Service
+sc config "diagsvc" start-disabled
+
+:: disables Connected User Experiences and Telemetry
+sc config "DiagTrack" start-disabled
+
+:: disables Diagnostic Policy Service
+sc config "DPS" start-disabled
+
+:: disables Microsoft Edge Update Service
+sc config "edgeupdate" start-disabled
+sc config "edgeupdatem" start-disabled
+
+:: disables Enterprise App Management Service
+sc config "EntAppSvc" start-disabled
+
+:: disables fax
+sc config "Fax" start-disabled
+
+:: disables File History Service
+sc config "fhsvc" start-disabled
+
+:: disables Windows Camera Frame Server
+sc config "FrameServer" start-disabled
+
+:: disables Microsoft FTP Service
+sc config "ftpsvc" start-disabled
+
+:: disables HV Hosting Service
+sc config "HvHost" start-disabled
+
+:: disables Windows Mobile Hotspot Service
+sc config "icssvc" start-disabled
+
+:: disables IP Helper
+sc config "iphlpsvc" start-disabled
+
+:: disables Geolocation Service
+sc config "lfsvc" start-disabled
+
+:: disables TCP/IP NetBIOS Helper
+sc config "lmhosts" start-disabled
+
+:: disables Downloaded Maps Manager
+sc config "MapsBroker" start-disabled
+
+:: disables Microsoft Edge Elevation Service
+sc config "MicrosoftEdgeElevationService" start-disabled
+
+:: disables Netlogon
+sc config "Netlogon" start-disabled
+
+:: disables Program Compatibility Assistant Service
+sc config "PcaSvc" start-disabled
+
+:: disables Phone Service
+sc config "PhoneSvc" start-disabled
+
+:: disables Printer Extensions and Notifications
+sc config "PrintNotify" start-disabled
+
+:: disables Remote Registry
+sc config "RemoteRegistry" start-disabled
+
+:: disables Smartcard
+sc config "SCardSvr" start-disabled
+
+:: disables Smartcard Device Enumeration Service
+sc config "ScDeviceEnum" start-disabled
+
+:: disables Smartcard Removal Policy
+sc config "SCPolicySvc" start-disabled
+
+:: disables Payments and NFC/SE Manager
+sc config "SEMgrSvc" start-disabled
+
+:: disables Remote Desktop Configuration
+sc config "SessionEnv" start-disabled
+
+:: disables Internet Connection Sharing
+sc config "SharedAccess" start-disabled
+
+:: disables Remote Desktop Services
+sc config "TermService" start-disabled
+
+:: disables Distributed Link Tracking Client
+sc config "TrkWks" start-disabled
+
+:: disables Remote Desktop Services UserMode Port Redirector
+sc config "UmRdpService" start-disabled
+
+:: disables Hyper-V Guest Service Interface
+sc config "vmicguestinterface" start-disabled
+
+:: disables Hyper-V Heartbeat Service
+sc config "vmicheartbeat" start-disabled
+
+:: disables Hyper-V Data Exchange Service
+sc config "vmickvpexchange" start-disabled
+
+:: disables Hyper-V Remote Desktop Virtualization Service
+sc config "vmicrdv" start-disabled
+
+:: disables Hyper-V Guest Shutdown Service
+sc config "vmicshutdown" start-disabled
+
+:: disables Hyper-V Time Synchonization Service
+sc config "vmictimesync" start-disabled
+
+:: disables Hyper-V PowerShell Direct Service
+sc config "vmicvmsession" start-disabled
+
+:: disables Hyper-V Volume Shadow Copy Requester
+sc config "vmicvss" start-disabled
+
+:: disables Windows Connect Now - Config Registrar
+sc config "wcncsvc" start-disabled
+
+:: disables Diagnostic Service Host
+sc config "WdiServiceHost" start-disabled
+
+:: disables Diagnostic System Host
+sc config "WdiSystemHost" start-disabled
+
+:: disables Problem Report Control Panel Support
+sc config "wercplsupport" start-disabled
+
+:: disables Windows Error Reporting
+sc config "WerSvc" start-disabled
+
+:: disables Windows Insider Service
+sc config "wisvc" start-disabled
+
+:: disables Windows Media Player Network Sharing Service
+sc config "WMPNetworkSvc" start-disabled
+
+:: disables Parental Control
+sc config "WpcMonSvc" start-disabled
+
+:: disables Security Center
+sc config "wscsvc" start-disabled
+
+:: disables Windows Search
+sc config "WSearch" start-disabled
+
+:: disables Xbox Live Auth Manager
+sc config "XblAuthManager" start-disabled
+
+:: disables Xbox Live Game Save
+sc config "XblGameSave" start-disabled
+
+:: disables Xbox Accessory Management Service
+sc config "XboxGipSvc" start-disabled
+
+:: disables Xbox Live Networking Services
+sc config "XboxNetApiSvc" start-disabled
+
+:: disables GameDVR and Broadcast
+sc config "BcastDVRUserService_82436" start-disabled
+
+:: disables OneSyncSvc_82436
+sc config "OneSyncSvc_82436" start-disabled
+
+:: OPTIONAL
+:: disables Print Spooler
+sc config "Spooler" start-disabled
+
+:: PrintWorkflow_873c8
+sc config "PrintWorkflowUserSvc_82436" start-disabled
+
+:: disables Windows Image Acquisition (for scanners)
+sc config "stisvc" start-disabled
+
+:: disables Windows Biometric Service
+::sc config "WbioSrvc" start-disabled
+
+:: disables Touch Keyboard and Handwriting Panel Service
+::sc config "TabletInputService" start-disabled
+
+:: disables Secondary Logon
+::sc config "seclogon" start-disabled
